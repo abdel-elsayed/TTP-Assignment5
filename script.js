49266418414afd40d6aa6eb8bbb64f04bcdf6f7c
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-08 16:02:56 -0500

    DEV: added a remove row button and script for it.

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ const grid = document.getElementById("grid1")
 let colNum = 3
 let rowNum = 3
 
-
-// adding a new row
+// Buttons to add and remove rows
 const addRow = document.getElementById("addRow")
+const remRow = document.getElementById("remRow")
 
-
+// Event listener to add rows
 addRow.addEventListener("click", addRowFunction);
 
+// Event listener to remove rows
+remRow.addEventListener("click", deleteRowFunction);
+
 function addRowFunction(){
-    let tempCol = colNum
-    while(tempCol > 0){
+    for(let i=0;i<colNum;i++ ){
         let newCell=document.createElement("div")
         newCell.classList.add("grid-item")
         newCell.innerText = "new-cell-row"
         grid.appendChild(newCell);
-        tempCol--;
     }
     rowNum++;
     console.log(rowNum)
 }
 
+function deleteRowFunction(){
+    for(let i=0;i<colNum;i++ ){
+        grid.removeChild(grid.lastChild);
+    }
+    rowNum--;
+    console.log(rowNum)
+}
