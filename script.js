864b8a2257c601a743793767e7962ed83664827f
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-08 16:30:19 -0500

    DEV: fixed the remove-row function

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ const grid = document.getElementById("grid1")
 let colNum = 3
 let rowNum = 3
 
-// Buttons to add and remove rows
+// getting the Buttons to add and remove rows
 const addRow = document.getElementById("addRow")
 const remRow = document.getElementById("remRow")
 
 // Event listener to add rows
-addRow.addEventListener("click", addRowFunction);
-
-// Event listener to remove rows
-remRow.addEventListener("click", deleteRowFunction);
-
-function addRowFunction(){
+addRow.addEventListener("click", function(){
     for(let i=0;i<colNum;i++ ){
         let newCell=document.createElement("div")
         newCell.classList.add("grid-item")
@@ -21,12 +16,13 @@ function addRowFunction(){
     }
     rowNum++;
     console.log(rowNum)
-}
+});
 
-function deleteRowFunction(){
-    for(let i=0;i<colNum;i++ ){
-        grid.removeChild(grid.lastChild);
+// Event listener to remove rows
+remRow.addEventListener("click", function(){
+    for(let i=0; i<colNum; i++ ){
+    grid.lastElementChild.remove()
     }
     rowNum--;
     console.log(rowNum)
-}
+})
